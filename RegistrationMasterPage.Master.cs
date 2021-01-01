721591e28272a21a8bb393374bc439bd4ea10988
Github-Professor <67437752+Github-Professor@users.noreply.github.com>
2021-01-02 00:53:11 +0530

    Created the Farmer Registration Form

---
+++ RegistrationMasterPage.Master.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_Project_02
+{
+    public partial class RegistrationMasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            txtAge.Text = "";
+            txtDistrict.Text = "";
+            txtMobile.Text = "";
+            txtName.Text = "";
+            txtNationalID.Text = "";
+            txtPassword.Text = "";
+            txtConfirmPassword.Text = "";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtAge.Text.Equals("") || txtDistrict.Text.Equals("") || txtMobile.Text.Equals("") || txtName.Text.Equals("") || txtNationalID.Text.Equals("") || txtPassword.Text.Equals("") || txtConfirmPassword.Text.Equals(""))
+            {
+                lblError1.Text = "Please enter information!";
+            }
+            else
+            {
+                // Put the regular expressions here.......
+                try
+                {
+                    string conStr = ConfigurationManager.ConnectionStrings["farmer"].ConnectionString;
+                    using (SqlConnection con = new SqlConnection(conStr))
+                    {
+                        con.Open();
+                        string queryStr = "INSERT INTO farmer VALUES (@national_id, @name, @age, @district, @mobile, @password)";
+                        using (SqlCommand cmd = new SqlCommand(queryStr, con))
+                        {
+                            cmd.Parameters.AddWithValue("national_id", txtNationalID.Text);
+                            cmd.Parameters.AddWithValue("name", txtName.Text);
+                            cmd.Parameters.AddWithValue("age", txtAge.Text);
+                            cmd.Parameters.AddWithValue("district", txtDistrict.Text);
+                            cmd.Parameters.AddWithValue("mobile", txtMobile.Text);
+                            cmd.Parameters.AddWithValue("password", txtPassword.Text);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                        con.Close();
+                    }
+                }
+                catch (SqlException)
+                {
+                    lblError.Text = "National ID is Already Used!";
+                }
+                catch (Exception ex)
+                {
+                    lblError1.Text = "Error Occured! " + ex;
+                }
+                finally
+                {
+
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ Registration_Page.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/RegistrationMasterPage.Master" AutoEventWireup="true" CodeBehind="Registration_Page.aspx.cs" Inherits="Web_Project_02.Registration_Page" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
