d7d8e41a68845fa40281d3539391b217c6c445a3
Mithina <64489364+Mithina@users.noreply.github.com>
2021-01-03 20:04:22 +0530

    Update ViewGraphReports.aspx.designer.cs

--- ViewGraphReports.aspx.designer.cs
+++ ViewGraphReports.aspx.designer.cs
@@ -31,5 +31,23 @@ namespace Web_Project_02
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// Chart1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.DataVisualization.Charting.Chart Chart1;
     }
 }
